0a823600e20eff1f362ed430e5da43e0cef174dc
MohammedNasser12 <mohammed_abdelnasser@yahoo.com>
2025-04-27 19:02:40 +0300

    Code enhancements

--- ProfileExplorer/Coordinators/TabBarCoordinator.swift
+++ ProfileExplorer/Coordinators/TabBarCoordinator.swift
@@ -12,7 +12,15 @@ final class TabBarCoordinator {
     func start() -> UITabBarController {
         let tabBarController = UITabBarController()
         
-        tabBarController.viewControllers = []
+        let profileVC = ProfileViewController()
+        let adsVC = AdsViewController()
+        let tagsVC = TagsViewController()
+        
+        profileVC.tabBarItem = UITabBarItem(title: "Profile", image: UIImage(systemName: "person"), tag: 0)
+        adsVC.tabBarItem = UITabBarItem(title: "Ads", image: UIImage(systemName: "megaphone"), tag: 1)
+        tagsVC.tabBarItem = UITabBarItem(title: "Tags", image: UIImage(systemName: "tag"), tag: 2)
+        
+        tabBarController.viewControllers = [profileVC, adsVC, tagsVC]
         
         return tabBarController
     }
